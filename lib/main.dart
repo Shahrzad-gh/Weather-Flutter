a5638a35a5305c2398b70eab5468cbd7ae97286e
Shahr Zad <gh.shahrzad@gmail.com>
2021-10-27 20:18:55 +0330

    add routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
-import 'pages/home.dart';
+import 'package:weather_flutter/pages/choose_location.dart';
+import 'package:weather_flutter/pages/loading.dart';
+import 'package:weather_flutter/pages/Home.dart';
 
-void main() => runApp(const MaterialApp(
-      home: Home(),
+void main() => runApp( MaterialApp(
+  initialRoute: '/home',
+      routes: {
+        '/':(context) => const Loading(),
+        '/home': (context) => const Home(),
+        '/location': (context) => const ChooseLocation(),
+      }
     ));
