54ef0665d5d221d2242d583a4dceab5165c58a32
Shahr Zad <gh.shahrzad@gmail.com>
2021-10-27 20:19:26 +0330

    test initState - build func - setState

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/material.dart';
 
 class ChooseLocation extends StatefulWidget {
-  const ChooseLocation({ Key? key }) : super(key: key);
+  const ChooseLocation({Key? key}) : super(key: key);
 
   @override
   _ChooseLocationState createState() => _ChooseLocationState();
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+  @override
+  void initState() {
+    super.initState();
+    print('initSate function run');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Choose location screen")
-    );
+    print('build function run');
+
+    return Scaffold(
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[300],
+          title: const Text('Choose a Location'),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: TextButton(
+          onPressed: () {
+            setState(() {
+              counter += 1;
+            });
+          },
+          child:
+            Text('Counter is $counter')
+        ));
   }
 }
