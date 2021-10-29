2116054fdb0016c90cfd1421650e2822052c50c0
Shahr Zad <gh.shahrzad@gmail.com>
2021-10-29 20:51:54 +0330

    update

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,8 +8,14 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Map data = {};
+
   @override
   Widget build(BuildContext context) {
+    
+    //data = ModalRoute.of(context)!.settings.arguments;
+    print(data);
+
     return Scaffold(
         body: SafeArea(
             child: Column(
@@ -20,6 +26,14 @@ class _HomeState extends State<Home> {
           },
           icon: const Icon(Icons.edit_location),
           label: const Text('Edit Location'),
+        ),
+        const SizedBox(height: 20.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(data['location'],
+                style: const TextStyle(fontSize: 20.0, letterSpacing: 2.0))
+          ],
         )
       ],
     )));
