c1efec8fa9c2d5cdc9603d824beb5043312681c4
Shahr Zad <gh.shahrzad@gmail.com>
2021-10-29 20:51:45 +0330

    update

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:weather_flutter/services/world_time.dart';
 
 class Loading extends StatefulWidget {
-  const Loading({ Key? key }) : super(key: key);
+  const Loading({Key? key}) : super(key: key);
 
   @override
   _LoadingState createState() => _LoadingState();
 }
 
 class _LoadingState extends State<Loading> {
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Brlin', url: 'Europe/Berlin');
+
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Text("loading screen")
+      body:
+          Padding(padding: const EdgeInsets.all(50.0), child: Text('loading')),
     );
   }
 }
