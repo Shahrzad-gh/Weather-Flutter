1d81081d1cf0ac157a08f85ec12e9aac2afacb68
Shahr Zad <gh.shahrzad@gmail.com>
2021-10-31 18:08:17 +0330

    add flag - isdaytime to class

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -5,10 +5,11 @@ import 'package:intl/intl.dart';
 class WorldTime {
   String location; //location name for the ui
   String? time; // the time in that location
-  String? flag; //url to an asset flag icon
+  String flag; //url to an asset flag icon
   String url; // location url endpont
+  bool? isDayTime; // true or false if daytime or not;
 
-  WorldTime({required this.location, required this.url});
+  WorldTime({required this.location, required this.flag, required this.url});
 
   Future<void> getTime() async {
     try {
@@ -22,6 +23,7 @@ class WorldTime {
       DateTime now = DateTime.parse(datetime);
       now = now.add(Duration(hours: int.parse(offset)));
       //set the time property
+      isDayTime =  now.hour > 6 && now.hour <20 ? true: false;
       time = DateFormat.jm().format(now);
     } catch (e) {
       print('caught error: $e');
