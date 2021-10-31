53e6aff4416fc5249cedb7984efdccb3d3635c28
Shahr Zad <gh.shahrzad@gmail.com>
2021-10-31 18:06:40 +0330

    fix bugs

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,34 +8,53 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  Map data = {};
+  Map? data = {};
 
   @override
   Widget build(BuildContext context) {
-    
-    //data = ModalRoute.of(context)!.settings.arguments;
+    data = ModalRoute.of(context)!.settings.arguments as Map?;
     print(data);
+
+    //set background
+  String bgImage = data!['isDayTime'] ? 'day.png' : 'night.png';
 
     return Scaffold(
         body: SafeArea(
-            child: Column(
-      children: <Widget>[
-        TextButton.icon(
-          onPressed: () {
-            Navigator.pushNamed(context, '/location');
-          },
-          icon: const Icon(Icons.edit_location),
-          label: const Text('Edit Location'),
-        ),
-        const SizedBox(height: 20.0),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(data['location'],
-                style: const TextStyle(fontSize: 20.0, letterSpacing: 2.0))
-          ],
-        )
-      ],
-    )));
+          child: Container(
+            // decoration: const BoxDecoration(
+            //   image: DecorationImage(
+            //     image: AssetImage('assets/bg/night.png'),
+            //     fit: BoxFit.cover
+            //   )
+            // ),
+            child: Padding(
+            padding: const EdgeInsets.fromLTRB(0,120.0,0,0),
+            child: SafeArea(
+                child: Column(
+                children: <Widget>[
+            TextButton.icon(
+              onPressed: () {
+                Navigator.pushNamed(context, '/location');
+              },
+              icon: const Icon(Icons.edit_location),
+              label: const Text('Edit Location'),
+            ),
+            const SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(data!['location'],
+                    style: const TextStyle(fontSize: 20.0, letterSpacing: 2.0))
+              ],
+            ),
+            const SizedBox(height: 20.0),
+            Text(
+              data!['time'],
+              style: const TextStyle(fontSize: 66.0)
+            )
+                ],
+              )),
+                  ),
+          )));
   }
 }
