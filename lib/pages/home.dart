5fff06fe1dd0c038e38cc6f3cbf9bab502b41210
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-01 20:25:38 +0330

    add listview ui

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,49 +12,60 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    data = ModalRoute.of(context)!.settings.arguments as Map?;
+    data = data!.isNotEmpty ?  data : ModalRoute.of(context)!.settings.arguments as Map?;
     print(data);
 
     //set background
-  String bgImage = data!['isDayTime'] ? 'day.png' : 'night.png';
-
+    String bgImage = data!['isDayTime'] ? 'day.png' : 'night.png';
+    Color bgColor = data!['isDayTime'] ? Colors.blue : Colors.indigo;
     return Scaffold(
+        backgroundColor: bgColor,
         body: SafeArea(
-          child: Container(
-            // decoration: const BoxDecoration(
-            //   image: DecorationImage(
-            //     image: AssetImage('assets/bg/night.png'),
-            //     fit: BoxFit.cover
-            //   )
-            // ),
-            child: Padding(
-            padding: const EdgeInsets.fromLTRB(0,120.0,0,0),
+            child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/$bgImage'), fit: BoxFit.cover)),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
             child: SafeArea(
                 child: Column(
-                children: <Widget>[
-            TextButton.icon(
-              onPressed: () {
-                Navigator.pushNamed(context, '/location');
-              },
-              icon: const Icon(Icons.edit_location),
-              label: const Text('Edit Location'),
-            ),
-            const SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text(data!['location'],
-                    style: const TextStyle(fontSize: 20.0, letterSpacing: 2.0))
-              ],
-            ),
-            const SizedBox(height: 20.0),
-            Text(
-              data!['time'],
-              style: const TextStyle(fontSize: 66.0)
-            )
-                ],
-              )),
+                TextButton.icon(
+                  onPressed: () async {
+                    dynamic result =
+                        await Navigator.pushNamed(context, '/location');
+                    setState(() {
+                      data = {
+                        'location': result['location'],
+                        'flag': result['flag'],
+                        'time': result['time'],
+                        'isDayTime': result['isDayTime']
+                      };
+                    });
+                  },
+                  icon: Icon(Icons.edit_location, color: Colors.grey[300]),
+                  label: const Text(
+                    'Edit Location',
+                    style: TextStyle(color: Colors.white),
                   ),
-          )));
+                ),
+                const SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(data!['location'],
+                        style: const TextStyle(
+                            fontSize: 20.0,
+                            letterSpacing: 2.0,
+                            color: Colors.white))
+                  ],
+                ),
+                const SizedBox(height: 20.0),
+                Text(data!['time'],
+                    style: const TextStyle(fontSize: 66.0, color: Colors.white))
+              ],
+            )),
+          ),
+        )));
   }
 }
