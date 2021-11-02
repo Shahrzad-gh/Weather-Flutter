283ba7cb1a72b602bc0c22a834c1044ca33b24b3
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-02 12:25:31 +0330

    update

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -18,7 +18,7 @@ class _LoadingState extends State<Loading> {
       'location': instance.location,
       'flag': instance.flag,
       'time': instance.time,
-      'isDayTime' : instance.isDayTime
+      'isDayTime' : instance.isDayTime      
     });
   }
 
